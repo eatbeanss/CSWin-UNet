75980363e56f9c65f12d2625bf2cc265c61805a4
liuxiao <105787170+eatbeanss@users.noreply.github.com>
2024-09-19 21:57:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -126,9 +126,7 @@ if __name__ == "__main__":
 
     log_folder = './test_log/test_log_'
     os.makedirs(log_folder, exist_ok=True)
-    # logging.basicConfig(filename=log_folder + '/'+snapshot_name+".txt", level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
-    logging.basicConfig(filename=log_folder + '/' + "epoch149.txt", level=logging.INFO,
-                        format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.basicConfig(filename=log_folder + '/'+snapshot_name+".txt", level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     logging.info(str(args))
     logging.info(snapshot_name)
